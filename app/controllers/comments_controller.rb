f3828548dc6b1aeae282f6dfe7b575fb0a44cf81
kevineo <kevin.eugene.ong@gmail.com>
2016-05-21 11:36:04 +0800

    comments can be destroyed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,7 @@ class CommentsController < ApplicationController
 
 	def destroy
 		@post = Post.find(params[:post_id])
-		@comment = @post.comments.where(params[:post_id])
+		@comment = @post.comments.find(params[:post_id])
 		@comment.destroy
 
 		redirect_to post_path(@post)
