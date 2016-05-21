5d9e5fbc84e73b7f6f11e3ee21c34c6176105eed
kevineo <kevin.eugene.ong@gmail.com>
2016-05-21 11:31:17 +0800

    comments working

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+	def index
+		@comments = Comment.find(params[:post_id])
+	end
+
+	def create
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.create(params[:comment].permit(:name, :body))
+
+		redirect_to post_path(@post)
+	end
+
+	def show
+		@post = Post.find(params[:post_id])
+		@comment = Comment.find([:post_id])
+	end
+
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.where(params[:post_id])
+		@comment.destroy
+
+		redirect_to post_path(@post)
+	end
+
+
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,46 @@
 class PostsController < ApplicationController
+
+
 	def index
 		@posts = Post.all.order('created_at DESC')
 	end
 
 	def new
+		@post = Post.new
+	end
+
+	
+	def show
+		@post = Post.find(params[:id])
 	end
 
 	def create
 		@post = Post.new(post_params)
-		@post.save
+		
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
 
-		redirect_to @post
+	def edit
+		@post = Post.find(params[:id])
 	end
 
 	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else
+			render 'edit'
+		end
 	end
 
-	def show
-		@post = Post.find(params[:id])
+	def destroy
 	end
+
 
 	private
 
